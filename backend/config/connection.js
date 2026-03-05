389afd721d2859df4e8e63f072415c90eb222c67
ZAK-2020 <59872069+ZAK-2020@users.noreply.github.com>
2026-03-05 10:58:01 +0500

    Update connection.js

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,18 +1,21 @@
-const{connect} = require("mongoose")
+const { connect } = require("mongoose");
 
 let isConnected;
 
-const connectDatabase = async () =>{
-    if(isConnected) return;
-    try {
-        console.log(process.env.DATABASE_URL)
-        await connect(process.env.DATABASE_URL).then((data)=>{
-            console.log(`Mongodb connected with server: ${data.connection.host}`)
-        });
-        isConnected = true; 
-    } catch (error) {
-        console.log("database not connected")
-    }
-}
+const connectDatabase = async () => {
+  if (isConnected) return;
+
+  try {
+    console.log("Connecting to MongoDB...");
+
+    const data = await connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected with server: ${data.connection.host}`);
+
+    isConnected = true;
+  } catch (error) {
+    console.log("database not connected", error.message);
+  }
+};
 
 module.exports = connectDatabase;
